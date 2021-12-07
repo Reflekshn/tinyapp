5b8dc3822d185a33c6c20aaa8b6a1b67220dec48
Reflekshn <reflekshn@hotmail.com>
2021-12-07 08:23:09 +0000

    Added a route handler for the shortURL's

--- express_server.js
+++ express_server.js
@@ -27,8 +27,14 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+  res.write(req.params.shortURL);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get('/hello', (req, res) => {
