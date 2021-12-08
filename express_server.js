35d02e8a276418b5132f6c6134241d4b7202ea38
Reflekshn <reflekshn@hotmail.com>
2021-12-08 20:05:18 +0000

    Added POST route handler for logout

--- express_server.js
+++ express_server.js
@@ -103,6 +103,11 @@ app.post('/login', (req, res) => {
   res.redirect('urls');
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('urls');
+});
+
 /////////////////////////////////////////
 // Helper Functions
 /////////////////////////////////////////
